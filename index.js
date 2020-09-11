6d619ec73de8be2adbfb46ae9b1b831f7e9090b7
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-12 06:31:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function luas(panjang, lebar) {
+   var luas = panjang * lebar;
+    return luas;
+}
+document.write('panjang : 12 <br/>')
+document.write('lebar : 16 <br/>')
+document.write('<br/>luas persegi panjang : ' + luas(12, 16))
